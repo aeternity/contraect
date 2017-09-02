60840c2a6a158674d8b8dbb59a38c230d86e98a5
Sascha Hanse <shanse@posteo.de>
2017-09-02 20:19:08 +0200

    allow 0 value token transfers

--- contracts/StandardToken.sol
+++ contracts/StandardToken.sol
@@ -18,7 +18,7 @@ contract StandardToken is Token {
         //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
         //Replace the if with this one instead.
         //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-				require(balances[msg.sender] >= _value && _value > 0);
+				require(balances[msg.sender] >= _value && _value >= 0);
 				balances[msg.sender] -= _value;
 				balances[_to] += _value;
 				Transfer(msg.sender, _to, _value);
@@ -28,7 +28,7 @@ contract StandardToken is Token {
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
         //same as above. Replace this line with the following if you want to protect against wrapping uints.
         //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-				require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0);
+				require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value >= 0);
 				balances[_to] += _value;
 				balances[_from] -= _value;
 				allowed[_from][msg.sender] -= _value;
